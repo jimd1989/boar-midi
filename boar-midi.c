4670f25d61882af9688c199c9832784aaf305b85
jimd1989 <jamesdalrymple@protonmail.com>
2020-05-24 21:12:53 +0900

    Some options added. Channel fifos dont work

--- boar-midi.c
+++ boar-midi.c
@@ -1,35 +1,62 @@
-#include <stdio.h>
+#include <err.h>
 #include <sndio.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BUFSIZE 64
 
-int main() {
-    uint8_t buf[BUFSIZE] = {0};
-    struct mio_hdl *m;
-    int i, read, channel, note, velocity = 0;
+typedef struct Settings {
+  char        * in;  
+  FILE        * out[16];
+  bool          noblock;
+} Settings;
 
-    m = mio_open("rmidi/0", MIO_IN, 0);
-    if (m == NULL) {
-        printf("Couldn't find MIDI device.\n");
-        return 1;
+Settings setChannel(Settings s, char *a, char *f) {
+  int ch = atoi(a);
+  if (ch < 1 || ch > 16) { errx(1, "Invalid channel %d", ch); }
+  s.out[ch - 1] = fopen(f, "w");
+  if (s.out[ch - 1] == NULL) { errx(1, "Failed to open %s", f); }
+  return s;
+}
+
+Settings readArgs(int argc, char **argv) {
+  Settings s = {0};
+  char *a = NULL;
+  int i = 0;
+  s.in = "rmidi/0";
+  for (; i < 16; i++) { s.out[i] = stdout; }
+  for (i = 1 ; i < argc ; i++) {
+    a = argv[i];
+    if      (strcmp(a, "-noblock") == 0) { s.noblock = true; }
+    else if (strcmp(a, "-input") == 0)   { s.in = argv[++i]; }
+    else if (a[0] == '-' && a[1] == 'c') { s = setChannel(s, a + 2, argv[++i]); }
+    else                                 { errx(1, "Invalid arg %s", a); }
+  }
+  return s;
+}
+
+int main(int argc, char **argv) {
+  uint8_t buf[BUFSIZE] = {0};
+  struct mio_hdl *m;
+  int i, read, ch, note, velocity = 0;
+  Settings s = readArgs(argc, argv);
+
+  m = mio_open(s.in, MIO_IN, s.noblock);
+  if (m == NULL) { errx(1, "Couldn't find MIDI device %s", s.in); }
+  while (1) {
+    read = mio_read(m, buf, BUFSIZE); 
+    for (i = 0; i < read; i++) {
+      ch = 144^buf[i];
+      if (ch < 16) {
+        note = buf[++i];
+        velocity = buf[++i];
+        if (!velocity) { fprintf(s.out[ch], "o%d\n", note); }
+        else           { fprintf(s.out[ch], "n%d\n", note | (velocity << 9)); }
+      }
     }
-    while (1) {
-        read = mio_read(m, buf, BUFSIZE); 
-        i = 0;
-        while (i < read) {
-            channel = 144^buf[i];
-            if (channel < 16) {
-                note = buf[++i];
-                velocity = buf[++i];
-                if (!velocity) {
-                    printf("o%d\n", note);
-                } else {
-                    printf("n%d\n", note | (velocity << 9));
-                }
-                fflush(stdout);
-            }
-            i++;
-        }
-    }
-    return 0;
+    fflush(stdout);
+  }
+  return 0;
 }
